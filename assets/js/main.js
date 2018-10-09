4a57f7a4a1eae4f99132df5c4b753042cb914f06
bytesandchips <19722390+bytesandchips@users.noreply.github.com>
2018-10-09 19:47:43 +0545

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 /*
-	Industrious by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+	Bytes And Chips Template
+	Released for free under the Creative Commons Attribution 3.0 license 
 */
 (function($) {
 
